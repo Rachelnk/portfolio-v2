de57c6e9602709ad19602ad40f85763b79f04d62
Rachel Kiarie <rachelnnk94@gmail.com>
2022-08-27 13:51:45 +0300

    Add technologies buttons in portfolio.

--- src/Pages/Education.js
+++ src/Pages/Education.js
@@ -3,15 +3,30 @@ import './Education.css';
 function Education(){
   return(
     <>
-    <div>
-    <div>
-      <h1>Education And Work Experience</h1>
-      <h2>CERTIFICATE - FULL STACK SOFTWARE DEVELOPMENT</h2> <br></br>
-      <button>Feb 2022 - July 2022</button> <br></br>
-      <button>Feb 2022 - July 2022</button>
-      <h3> <a href='https://moringaschool.com/'> Moringa School</a></h3>
+     <h1>Education And Work Experience</h1>
+         
+    <div className='row'>
+      <h1>Education</h1>
+      <div className='col-md-6'> 
+        <div className=''>
+          <h2>CERTIFICATE - FULL STACK SOFTWARE DEVELOPMENT</h2> <br></br>
+          <button className='edu-btn'>Feb 2022 - July 2022</button> <br></br>      
+            <h3> <a href='https://moringaschool.com/'> Moringa School</a></h3>
 
-    </div>
+        </div>
+      
+        <div className=''>
+          <h2>B.SC. BUSINESS INFORMATION TECHNOLOGY</h2> <br></br>
+          <button className='edu-btn'>Sept 2013 - June 2017</button> <br></br>      
+            <h3> <a href='http://www.jkuat.ac.ke/'> Jomo Kenyatta University Of Agriculture and Technology</a></h3>
+            <h2>Attained a Second-Class Upper Honours</h2>
+
+        </div>
+      </div>
+      <h1>Work Experience</h1>
+      <div className='col-md-6'>
+        
+      </div>
 
     </div>
     
--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -1,4 +1,17 @@
-function Portfolio () {}
-
+import './Portfolio.css'
+function Portfolio () {
+  return(
+    <>
+    <h1>PORTFOLIO</h1>
+    <p>Here are some of the solo projects as well as those that I’ve collaborated on worked on. </p> 
+    <button className="tech">ALL</button> <button className="tech">Angular</button>
+    <button className="tech">React</button> <button className="tech">Django</button>
+    <button className="tech">Flask</button> <button className="tech">HTML, CSS,<br></br> Javascript</button>
+    <button className="tech">Full Stack</button>  
+    
+    </>
+  )
+}
+  
  
  export default Portfolio;
