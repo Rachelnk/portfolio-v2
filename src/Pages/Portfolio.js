c7d59b2816de1f59a46ca64be40dd03bfbe780f4
Rachel Kiarie <rachelnnk94@gmail.com>
2022-08-27 20:42:56 +0300

    Install bootstrap for react.

--- src/Pages/Portfolio.js
+++ src/Pages/Portfolio.js
@@ -3,11 +3,17 @@ function Portfolio () {
   return(
     <>
     <h1>PORTFOLIO</h1>
+    <div>
     <p>Here are some of the solo projects as well as those that I’ve collaborated on worked on. </p> 
     <button className="tech">ALL</button> <button className="tech">Angular</button>
     <button className="tech">React</button> <button className="tech">Django</button>
     <button className="tech">Flask</button> <button className="tech">HTML, CSS,<br></br> Javascript</button>
-    <button className="tech">Full Stack</button>  
+    <button className="tech">Full Stack</button> 
+    </div>
+    <div className='row'>
+
+    </div>
+     
     
     </>
   )
