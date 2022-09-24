59eef3f8255491bf4bd3adce19be05d15a11defd
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-25 00:48:06 +0300

    Add card class styling in app.css.

--- src/Pages/Contact.js
+++ src/Pages/Contact.js
@@ -11,8 +11,8 @@ function ContactMe () {
     <h3 className='contact-info'style={{fontSize: '20px'}}>Glad you made it this far.</h3>
       <p className='contact-info' style={{fontSize: '25px', margin: '20px'}}>I do hope my portfolio up until this point was not a waste of your time. Do consider me for future jobs and opportunities.</p>
      <div className='text-center' style={{paddingBottom: '25px'}}>
-     <a  href='https://github.com/Rachelnk' > <img src={linkedin} alt='' width='40' style={{marginBottom:'10px'}}/> </a>
-        <a href='https://www.linkedin.com/in/rachael-kiarie/'><img src={github} alt='' width='40'/> </a>
+     <a  href='https://www.linkedin.com/in/rachael-kiarie/' > <img src={linkedin} alt='' width='40' style={{marginRight:'15px'}}/> </a>
+        <a href='https://github.com/Rachelnk'><img src={github} alt='' width='40' style={{marginRight:'15px'}}/> </a>
         <a href='mailto:rachelnk94@gmail.com'><img src={email} alt='' width='40'/></a>
        
       
