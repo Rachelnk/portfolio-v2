11a97c26375e7d17f92624218c9a74fff3f1f0c6
Rachel Kiarie <rachelnnk94@gmail.com>
2022-09-24 19:17:09 +0300

    Change linkedin and github link.

--- src/App.js
+++ src/App.js
@@ -52,10 +52,9 @@ function App() {
         <div id="contact"><ContactMe /></div>
       </div>
       <div className='social-icons w3-animate-bottom'>
-      <span style={{color: 'white'}}><a href='https://github.com/Rachelnk' > <img src={linkedin} alt='' width='40' style={{marginBottom:'10px'}}/> </a></span>
-        <a href='https://www.linkedin.com/in/rachael-kiarie/'><img src={github} alt='' width='40'/> </a>
-        <a href='mailto:rachelnk94@gmail.com'><img src={email} alt='' width='40'/></a>
-        <div class="line" style={{color: 'white'}}></div>
+      <a href='https://www.linkedin.com/in/rachael-kiarie/' > <img src={linkedin} alt='' width='40' style={{marginBottom:'10px'}}/> </a>
+        <a href='https://github.com/Rachelnk'><img src={github} alt='' width='40'style={{marginBottom:'10px'}}/> </a>
+        <a href='mailto:rachelnk94@gmail.com'><img src={email} alt='' width='40'/></a>        
       </div>
      {/* </div> */}
      
